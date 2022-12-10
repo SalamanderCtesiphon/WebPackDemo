d9b88fb36e47fb44cf3b9fbc513038954753b7c5
SalamanderCtesiphon <swbrookshire@gmail.com>
2022-12-10 12:36:26 -0600

    added a png background to the bundle

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 import _ from 'lodash';
 import { myName } from './myName';
 import './style.css';
+import Icon from './icon.png';
 
 function component() {
     const element = document.createElement('div');
   
     element.textContent = myName('John');
     element.classList.add('hello');
+
+    // Add the image to our existing div.
+    const myIcon = new Image();
+    myIcon.src = Icon;
+
+    element.appendChild(myIcon);
+
     return element;
   }
   
