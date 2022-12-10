5c0e4c1567fd79889fd3ba0396a20ba4bc267eec
SalamanderCtesiphon <swbrookshire@gmail.com>
2022-12-10 12:23:56 -0600

    added style

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import _ from 'lodash';
 import { myName } from './myName';
+import './style.css';
 
 function component() {
     const element = document.createElement('div');
   
-    
     element.textContent = myName('John');
-  
+    element.classList.add('hello');
     return element;
   }
   
