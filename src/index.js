91447e4679669877cddbb73ed2174d113432039a
SalamanderCtesiphon <swbrookshire@gmail.com>
2022-12-10 12:48:45 -0600

    cleaning up the file for the next tutorial

--- src/index.js
+++ src/index.js
@@ -1,20 +1,9 @@
 import _ from 'lodash';
 import { myName } from './myName';
-import './style.css';
-import Icon from './icon.png';
 
 function component() {
     const element = document.createElement('div');
-  
-    element.textContent = myName('John');
-    element.classList.add('hello');
-
-    // Add the image to our existing div.
-    const myIcon = new Image();
-    myIcon.src = Icon;
-
-    element.appendChild(myIcon);
-
+    element.textContent = myName('John');   
     return element;
   }
   
